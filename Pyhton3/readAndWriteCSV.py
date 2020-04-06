f787f4f56fed85d57508673ec587d1cffaa2af08
beruntungbesar29 <60081725+WishMeLuck29@users.noreply.github.com>
2020-04-06 20:21:45 +0700

    Update readAndWriteCSV.py
    i add codes to read a csv file
    adding an error handling, so when the file doesn't exist
    the program will ask for the file name again
    the program (when i tested) aren't broken even if the file names given are ugly

--- Pyhton3/readAndWriteCSV.py
+++ Pyhton3/readAndWriteCSV.py
@@ -1,9 +1,36 @@
 # i want make a py program
 # that can read CSV file
 
+# module
+
 import csv
 
-with open('testReadCSV.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+
+#functions definision
+
+def readCSV ():
+
+	found = False
+
+	while (found == False):
+		try:
+			csv_name = input("Input your file name (with the extension) : ")
+			with open(csv_name, newline='') as f:
+			    reader = csv.reader(f)
+			    for row in reader:
+			        print(row)
+	        
+		except:
+			print ("the file can\'t be found!")
+			found = False
+
+		else:
+			found = True
+			f.close()
+			
+
+	
+
+#main program
+
+readCSV()
