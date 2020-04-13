cb37cadf06b49cc221e5cb92c0542005fe74da35
beruntungbesar29 <60081725+WishMeLuck29@users.noreply.github.com>
2020-04-13 12:18:16 +0700

    Create introduction.py

---
+++ Pyhton3/learn/introduction.py
@@ -0,0 +1,21 @@
+#Pengenalan pyhton
+"""
+Python dapat digunakan untuk:
+	pengembangan web
+	pengembangan software
+	matematik
+	dan system scripting
+
+Python dapat melakukan:
+	membuat aplikasi web di sebuah server
+	dapat digunakan bersama software untuk membuat diagram kerja
+	bisa digunakan dalam sistem database
+	mengerjakan perhitungan kompleks, dan menghandle data yang besar
+	membuat prototipe ataupun langsung digunakan untuk membuat software yang siap produksi
+
+Keunggulan Python:
+	bekerja di banyak platform 
+	sintaksnya singkat dan mudah dipahami
+	kode dapat dieksekusi segera setelah ditulis, karena menggunakan sistem interpreter
+	bisa digunakan secara prosedural ataupun OOP
+"""
